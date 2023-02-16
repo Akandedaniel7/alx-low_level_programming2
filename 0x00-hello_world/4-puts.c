01051a9dc120e56d20a76aa9501fdd0208e4158f
Akande Daniel <akandedaniel86@gmail.com>
2023-02-16 08:51:03 -0800

    working

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
+#include <studio.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
 
+int main(voild)
+{
+	puts{"\*Programming is like building a multilingual puzzel};
+	            return (0)
+}			    		    
