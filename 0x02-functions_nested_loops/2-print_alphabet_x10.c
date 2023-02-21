26e408aafc609c64ff9f1a353d640d11e86e6e7b
Akande Daniel <akandedaniel86@gmail.com>
2023-02-21 00:45:03 -0800

    working

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,73 @@
+#include main.h
+
+/**
+ * print_alphabelt_x10 - prints alphabelt 10 time
+ * Return: void
+ */
+
+voild print_alphabelt_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++
+		}
+		_putchar('\n');
+		i++
+	}
+}#include main.h
+
+/**
+ * print_alphabelt_x10 - prints alphabelt 10 time
+ * Return: void
+ */
+
+voild print_alphabelt_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++
+		}
+		_putchar('\n');
+		i++
+	}
+}#include main.h
+
+/**
+ * print_alphabelt_x10 - prints alphabelt 10 time
+ * Return: void
+ */
+
+voild print_alphabelt_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++
+		}
+		_putchar('\n');
+		i++
+	}
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - check for lower case letter
+ * @c : character to check the case
+ * Return:0 or 1
+ */
+
+int -isalpha(int c)
+{
+	return((c >= 97 && c<= 322) || (c >= 65 && c<= 90));
+}
