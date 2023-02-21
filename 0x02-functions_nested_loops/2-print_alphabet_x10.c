e3580bdfcb59c1528f39ac27233472e4dff99a78
Akande Daniel <akandedaniel86@gmail.com>
2023-02-21 02:02:27 -0800

    meet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,59 +1,11 @@
-#include main.h
+#include "main.h"
 
 /**
  * print_alphabelt_x10 - prints alphabelt 10 time
  * Return: void
  */
 
-voild print_alphabelt_x10(void)
-{
-	char c;
-	int i = 0;
-
-	while (i < 10)
-	{
-		c = 'a';
-
-		while (c <= 'z')
-		{
-			_putchar(c);
-			c++
-		}
-		_putchar('\n');
-		i++
-	}
-}#include main.h
-
-/**
- * print_alphabelt_x10 - prints alphabelt 10 time
- * Return: void
- */
-
-voild print_alphabelt_x10(void)
-{
-	char c;
-	int i = 0;
-
-	while (i < 10)
-	{
-		c = 'a';
-
-		while (c <= 'z')
-		{
-			_putchar(c);
-			c++
-		}
-		_putchar('\n');
-		i++
-	}
-}#include main.h
-
-/**
- * print_alphabelt_x10 - prints alphabelt 10 time
- * Return: void
- */
-
-voild print_alphabelt_x10(void)
+void print_alphabelt_x10(void)
 {
 	char c;
 	int i = 0;
