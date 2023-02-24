fe634b3b1bb2c3bf7c044c5ec8427d0caf06709c
Akande Daniel <akandedaniel86@gmail.com>
2023-02-24 07:53:16 -0800

    working

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,44 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_triangle - function declaration
- * Description: prints a triangle
- * @size: triangle height
+ * prime_factors - function declaration
+ * Description: finds largest prime factor
+ * @test_num: number to test
+ * Return: void
+ */
+
+void prime_factors(long test_num);
+
+/**
+ * main - entry point
+ * Description: calls prime_factors
  * Return: 0
  */
 
-void print_triangle(int size);
-
-/**
- * print_triangle - function definition
- * Description: prints right angle triangle
- * @size: triangle height in dashes
- * Return: 0
- */
-
-void print_triangle(int size)
+int main(void)
 {
-	int range;
-	int space;
-	int hash;
-
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (range = 1; range <= size; range++)
-		{
-			for (space = size - range; space > 0; space--)
-			{
-				_putchar(' ');
-			}
-			for (hash = 0; hash < range; hash++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
+	prime_factors(612852475143);
+	return (0);
 }
+
